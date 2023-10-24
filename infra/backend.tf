74f358809056228dfd5d7b3f7941add21920b232
Justin Lim <goodaccoustics@hotmail.com>
2023-10-25 00:21:49 +0800

    update working directory

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce3-tfstate-bucket-1"
-    key = "justinlim-ce-terraform-game2048-backend-bucket"
+    key = "justinlim-ce-terraform-game2048-backend"
     region = "us-east-1"
   }
 }
