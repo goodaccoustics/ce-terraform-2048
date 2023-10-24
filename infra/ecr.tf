728a3bfa59d735455b69ec61e7db716d4bf4b683
Justin Lim <goodaccoustics@hotmail.com>
2023-10-25 00:09:15 +0800

    rename ecr

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  name = "justinlim-ce-terraform-game2048"
+  name = "justinlim-ce-terraform-game2048-ecr"
   image_scanning_configuration {
     scan_on_push = true
   }
